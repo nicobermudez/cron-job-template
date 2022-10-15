60fc62a1e61f0e20ccdcfd1595680e18c10850c9
nicobermudez <bermnicolas@gmail.com>
2022-10-15 10:25:21 -0400

    init

--- src/bot.ts
+++ src/bot.ts
@@ -1,25 +1,8 @@
-import { AxiosInstance } from "axios";
 import cron, { ScheduledTask } from "node-cron";
-import { v4 } from "uuid";
 
-const task = (kucoin: AxiosInstance): ScheduledTask =>
+const task = (): ScheduledTask =>
     cron.schedule("* * * * *", async () => {
         console.log("bot initializing...");
-
-        // 1. find orders
-        // 2. establish rules to determine whether or not we want to buy
-
-        // 3. place orders
-
-        // example:
-        // await kucoin.post("/orders", {
-        //     clientOid: v4(),
-        //     side: "buy",
-        //     symbol: "ETH",
-        //     type: "market",
-        //     size: 10,
-        // });
-
         console.log("bot finished 🚀");
     });
 
